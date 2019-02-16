ad2e338d030b28ababbb33fee4e5aef126763522
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2019-02-16 04:28:31 -0600

    first commit

---
+++ js/calendarArray.js
@@ -0,0 +1,96 @@
+let calendarActivities = {
+    "Semana 1": [
+	    {
+	        date: "18 de Marzo",
+	        topic: "Introducción",
+	        subthemes: [
+	            "1 hora de plática de Introducción",
+	            "2 horas de ejercicios de integración"
+	        ]
+	    }, {
+	    	date:"19 de Marzo",
+	    	topic:"Pensamento estructurado",
+	    	subthemes:[
+	    		"Plática de pensamiento estructurado",
+	    		"Ejercicios de aplicación de pensamiento"
+	    	]
+	    }, {
+	    	date:"20 de Marzo",
+	    	topic:"Diagramas de flujo",
+	    	subthemes:[
+	    		"Explicación de diagramas de flujo",
+	    		"Ejercicios de aplicación de diagramas de flujo"
+	    	]
+	    } , {
+	    	date:"21 de Marzo",
+	    	topic:"Pensamiento lógico",
+	    	subthemes:[
+	    		"Plática sobre pensamiento lógico aplicado a programación",
+	    		"Ejercicios de pensamiento lógico"
+	    	]
+	    } , {
+	    	date:"22 de Marzo",
+	    	topic:"Pseudocódigo",
+	    	subthemes:[
+	    		"Explicación sobre Pseudocódigo",
+	    		"Ejercicios de aplicación de Pseudocódigo"
+	    	]
+	    }, {
+	    	date:"23 de Marzo",
+	    	topic:"Rally propedéutico",
+	    	subthemes:[
+	    		"N/A"
+	    	]
+	    }
+	],
+	 "Semana 2": [
+	    {
+	        date: "25 de Marzo", 
+	        topic: "Introducción a HTML y CSS",
+	        subthemes: [
+	            "Marco teórico HTML",
+	            "Etiquetas y atributos HTML",
+	            "Semántica HTML 5",
+	            "Maquetado de estructuras báscias con HTML"
+	        ]
+	    }, {
+	    	date:"26 de Marzo",
+	    	topic:"Introducción a HTML y CSS",
+	    	subthemes:[
+	    		"Marco teórico CSS",
+	    		"Creación y asignación de hojas de estilos",
+	    		"Selectores primarios (Tagname, Id, Clase)",
+	    		"Selectores anidados",
+	    		"Selectores concatenados",
+	    		"Pseudoselectores (evento, atributo, posición, ::after, ::before)"
+	    	]
+	    }, {
+	    	date:"27 de Marzo",
+	    	topic:"Introducción a HTML y CSS",
+	    	subthemes:[
+	    		"Media queries",
+	    		"Breakpoints",
+	    		"Desarrollo de clases responsivas"
+	    	]
+	    } , {
+	    	date:"28 de Marzo",
+	    	topic:"Introducción a HTML y CSS",
+	    	subthemes:[
+	    		"Transiciones con CSS",
+	    		"Animaciones con CSS"
+	    	]
+	    } , {
+	    	date:"29 de Marzo",
+	    	topic:"Rally HTML y CSS",
+	    	subthemes:[
+	    		"N/A"
+	    	]
+	    }, {
+	    	date:"30 de Marzo",
+	    	topic:"Estructura de Datos y Algoritmos",
+	    	subthemes:[
+	    		"N/A"
+	    	]
+	    }
+	]
+}
---
+++ js/main.js
@@ -0,0 +1,52 @@
+console.log(calendarActivities)
+
+var weekCount = 1;
+$.each(calendarActivities, function(index, value) {
+
+
+    console.log(weekCount)
+    $("#calendar-accordion>.card").append(`
+        <div class="card-header" id="${index}">
+            <h5 class="mb-0">
+            <button class="btn btn-link" data-toggle="collapse" data-target="#collapse-${weekCount}" aria-expanded="true" aria-controls="collapse-${weekCount}">
+            ${index}
+            </button>
+            </h5>
+        </div>
+        <div id="collapse-${weekCount}" class="collapse show" aria-labelledby="${index}" data-parent="#calendar-accordion">
+            <div class="card-body d-flex justify-content-between day-wrapper">
+                
+            </div>
+        </div>
+    `)
+    $.each(value, function(index, value) {
+        let currentDay = index;
+        $("#collapse-" + weekCount).find(".day-wrapper").append(`
+            <div class="card day-schedule">
+                <div class="card-body">
+                    <h5 class="card-title">${value.date}</h5>
+                    <p class="card-text">${value.topic}</p>
+                </div>
+                <ul class="list-group list-group-flush activities-detail-${index}"></ul>
+            </div>
+
+        `)
+        let subthemes = value.subthemes;
+        $.each(subthemes,function(index,value){
+            console.log(value);
+            $("#collapse-" + weekCount).find(".activities-detail-"+currentDay).append(`
+                <li class="list-group-item">${value}</li>
+            `)
+        })
+        /*for(i=0; i=subthemes.length; i++){
+            console.log(subthemes[i])
+            $("#collapse-" + weekCount).find(".activities-detail").append(`
+                <li class="list-group-item">${value.subthemes[i]}</li>
+            `)
+        }*/
+    })
+
+    weekCount = weekCount + 1
+
+});
+
