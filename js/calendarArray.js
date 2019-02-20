083db030c07e9ae38bcf71247beeca74a1338fce
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2019-02-20 15:17:34 -0600

    week 7

--- js/calendarArray.js
+++ js/calendarArray.js
@@ -68,6 +68,7 @@ let calendarActivities = {
 	    	date:"27 de Marzo",
 	    	topic:"Introducción a HTML y CSS",
 	    	subthemes:[
+	    		"Teoría sobre desarrollo responsivo",
 	    		"Media queries",
 	    		"Breakpoints",
 	    		"Desarrollo de clases responsivas"
@@ -76,6 +77,8 @@ let calendarActivities = {
 	    	date:"28 de Marzo",
 	    	topic:"Introducción a HTML y CSS",
 	    	subthemes:[
+	    		"Flexbox",
+	    		"Grid ",
 	    		"Transiciones con CSS",
 	    		"Animaciones con CSS"
 	    	]
@@ -88,6 +91,246 @@ let calendarActivities = {
 	    }, {
 	    	date:"30 de Marzo",
 	    	topic:"Estructura de Datos y Algoritmos",
+	    	subthemes:[
+	    		"N/A"
+	    	]
+	    }
+	],
+	"Semana 3": [
+	    {
+	        date: "1 de Abril", 
+	        topic: "Bootstrap",
+	        subthemes: [
+	            "Bootstrap - Overview",
+	            "Bootstrap Grid",
+	            "Bootstrap Components",
+	            "Maquetado responsivo empleando Bootstrap Grid",
+	            "Personalización y funcionalidad de componentes"
+	        ]
+	    }, {
+	    	date:"2 de Abril",
+	    	topic:"Bootstrap",
+	    	subthemes:[
+	    		"Clases responsivas",
+	    		"Clases de soporte",
+	    		"Clases de alineación"
+	    	]
+	    }, {
+	    	date:"3 de Abril",
+	    	topic:"Diseño de CV para perfiles de alto impacto",
+	    	subthemes:[
+	    		"Elementos que conforman un CV de alto impacto",
+	    		"Elementos esenciales para un perfil de linkedin de alto impacto"
+	    	]
+	    } , {
+	    	date:"4 de Abril",
+	    	topic:"Diseño de CV para perfiles de alto impacto",
+	    	subthemes:[
+	    		"Elementos esenciales de un CV",
+	    		"Preparación para entrevistas de trabajo efectivas"
+	    	]
+	    } , {
+	    	date:"5 de Abril",
+	    	topic:"Laboratorio HTML, CSS, Bootstrap",
+	    	subthemes:[
+	    		"Desafío 1"
+	    	]
+	    }, {
+	    	date:"6 de Abril",
+	    	topic:"UX / UI",
+	    	subthemes:[
+	    		"N/A"
+	    	]
+	    }
+	],
+	"Semana 4": [
+	    {
+	        date: "8 de Abril", 
+	        topic: "Introducción a JavaScript",
+	        subthemes: [
+	            "Marco teórico de Javascript",
+	            "Despliegue de datos",
+	            "Operadores de asignación",
+	            "Operadores aritméticos",
+	            "Operadores de comparación",
+	            "Operadores lógicos",
+	            "Tipos de datos",
+	            "Sentencias condicionales",
+	            "Ciclos"
+	        ]
+	    }, {
+	    	date:"9 de Abril",
+	    	topic:"Introducción a JavaScript",
+	    	subthemes:[
+	    		"Métodos de String",
+	    		"Código Hexadecimal en String",
+	    		"Código UNICODE",
+	    		"Introducción a métodos de Arrays",
+	    		"Funciones",
+	    		"Parámetros y argumentos"
+	    	]
+	    }, {
+	    	date:"10 de Abril",
+	    	topic:"Introducción a JavaScript",
+	    	subthemes:[
+	    		"Métodos en Arrays, continuación",
+	    		"Métodos de Arrays funcionales",
+	    		"Introducción a objetos en Javascript"
+	    	]
+	    } , {
+	    	date:"11 de Abril",
+	    	topic:"Introducción a JavaScript",
+	    	subthemes:[
+	    		"Objetos en JavaScript, continuación",
+	    		"Arrays de objetos",
+	    		"Métodos en JS",
+	    		"Funciones constructoras de objetos",
+	    		"Instanciación de objetos"
+	    	]
+	    } , {
+	    	date:"12 de Abril",
+	    	topic:"Rally GIT y JS",
+	    	subthemes:[
+	    		"N/A"
+	    	]
+	    }, {
+	    	date:"13 de Abril",
+	    	topic:"Por definir",
+	    	subthemes:[
+	    		"N/A"
+	    	]
+	    }
+	],
+	"Semana 5": [
+	    {
+	        date: "15 de Abril", 
+	        topic: "JavaScript",
+	        subthemes: [
+	            "Métodos"
+	        ]
+	    }, {
+	    	date:"16 de Abril",
+	    	topic:"JavaScript",
+	    	subthemes:[
+	    		"Eventos",
+	    		"Event Listeners",
+	    		"Carga de contenidos"
+	    	]
+	    }, {
+	    	date:"17 de Abril",
+	    	topic:"JavaScript",
+	    	subthemes:[
+	    		"Eventos DOM",
+	    		"Eventos DOM HTML"
+	    	]
+	    } , {
+	    	date:"18 de Abril",
+	    	topic:"JavaScript",
+	    	subthemes:[
+	    		"AJAX",
+	    		"Objeto XHTTP",
+	    		"Lectura de archivos JSON"
+	    	]
+	    } , {
+	    	date:"19 de Abril",
+	    	topic:"Rally JS",
+	    	subthemes:[
+	    		"N/A"
+	    	]
+	    }, {
+	    	date:"20 de Abril",
+	    	topic:"Por definir",
+	    	subthemes:[
+	    		"N/A"
+	    	]
+	    }
+	],
+	"Semana 6": [
+	    {
+	        date: "29 de Abril", 
+	        topic: "JavaScript",
+	        subthemes: [
+	            "AJAX, continuación",
+	            "Envío de datos",
+	            "Ejercicios"
+	        ]
+	    }, {
+	    	date:"30 de Abril",
+	    	topic:"JavaScript",
+	    	subthemes:[
+	    		"Uso del estándar ES6",
+	    		"Variables let y const",
+	    		"Array functions"
+	    	]
+	    }, {
+	    	date:"1 de Mayo",
+	    	topic:"JavaScript",
+	    	subthemes:[
+	    		"Interpolación",
+	    		"Parámetros por defecto",
+	    		"Operador Spread",
+	    		"Estructuración de objetos"
+	    	]
+	    } , {
+	    	date:"2 de Mayo",
+	    	topic:"JavaScript",
+	    	subthemes:[
+	    		"Ejercicios de lógica y aplicación de JS"
+	    	]
+	    } , {
+	    	date:"3 de Mayo",
+	    	topic:"Desafío 2",
+	    	subthemes:[
+	    		"Continuación de desafío con JS"
+	    	]
+	    }, {
+	    	date:"4 de Mayo",
+	    	topic:"Por definir",
+	    	subthemes:[
+	    		"N/A"
+	    	]
+	    }
+	],
+	"Semana 7": [
+	    {
+	        date: "6 de Mayo", 
+	        topic: "JQuery",
+	        subthemes: [
+	            "Teoría de JQuery",
+	            "Selectores",
+	            "Traversing",
+	            "Métodos de estilo con JQuery",
+	            "Métodos de animación con JQuery"
+	        ]
+	    }, {
+	    	date:"7 de Mayo",
+	    	topic:"JQuery",
+	    	subthemes:[
+	    		"Creación de objetos JSON con JQuery",
+	    		"AJAX",
+	    		"Manipulación de DOM y visualización de datos"
+	    	]
+	    }, {
+	    	date:"8 de Mayo",
+	    	topic:"Desafío Módulo 1",
+	    	subthemes:[
+	    		"N/A"
+	    	]
+	    } , {
+	    	date:"9 de Mayo",
+	    	topic:"Desafío Módulo 1",
+	    	subthemes:[
+	    		"N/A"
+	    	]
+	    } , {
+	    	date:"10 de Mayo",
+	    	topic:"Desafío Módulo 1",
+	    	subthemes:[
+	    		"N/A"
+	    	]
+	    }, {
+	    	date:"11 de Mayo",
+	    	topic:"Por definir",
 	    	subthemes:[
 	    		"N/A"
 	    	]
